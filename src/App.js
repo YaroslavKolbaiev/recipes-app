ca920eaceada83b1747a123bb66fca00d08d29eb
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2023-01-08 19:48:16 +0300

    added CD

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
 
-function App() {
+import firebase from './FirebaseConfig';
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='hero is-fullheight hero-background'>
+      <div className='hero-body'>
+        <h1 className='title'>Firebase App</h1>
+      </div>
+    </section>
+  )
 }
-
-export default App;
